baae62b4f3cb67a2f3318d02dbf79f5d94d6232f
Scott Zhao <zhaomh1998@outlook.com>
2019-06-01 00:59:44 -0700

    ios map working

--- APP/easy_stroll_mobile/ios/Runner/AppDelegate.m
+++ APP/easy_stroll_mobile/ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#include "GoogleMaps/GoogleMaps.h"
 
 @import UIKit;
 @import Firebase;
@@ -8,6 +9,7 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [FIRApp configure];
+  [GMSServices provideAPIKey:@"GOOGLE_MAP_API_KEY_HERE"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
